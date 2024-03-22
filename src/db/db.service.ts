faf84e068a03a81832e73f593901fca14c1d8b77
amberlynn <amberlynn364@gmail.com>
2024-03-22 18:36:10 +0300

    feat: add prisma and update docker-compose

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { TemporaryDatabase } from 'src/temporaryDatabase/temporaryDatabase';
 import { Album, Artist, Favorites, Track, User } from 'src/types';
 
 @Injectable()
+// export class DbService extends PrismaClient implements OnModuleInit {
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+// }
 export class DbService {
   public user = new TemporaryDatabase<User>();
   public track = new TemporaryDatabase<Track>();
